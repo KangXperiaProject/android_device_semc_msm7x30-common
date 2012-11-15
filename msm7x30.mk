d048d699be1d26eb6f04e0f25db6c4a9b0f6f454
Arnav Gupta <championswimmer@gmail.com>
2012-11-15 17:37:54 +0530

    filler not needed
    Change-Id: I371078873e26ca67861977c5bb35455379fd9492
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- msm7x30.mk
+++ msm7x30.mk
@@ -42,7 +42,6 @@ PRODUCT_COPY_FILES += \
 
 
 PRODUCT_COPY_FILES += \
-    device/semc/msm7x30-common/prebuilt/fillers:root/filler \
     device/semc/msm7x30-common/prebuilt/media_profiles.xml:system/etc/media_profiles.xml \
     device/semc/msm7x30-common/prebuilt/media_codecs.xml:system/etc/media_codecs.xml \
     device/semc/msm7x30-common/prebuilt/audio_policy.conf:system/etc/audio_policy.conf \
@@ -57,7 +56,8 @@ PRODUCT_COPY_FILES += \
     device/semc/msm7x30-common/prebuilt/nandroid-md5.sh:root/sbin/nandroid-md5.sh
 
 
-#    device/semc/msm7x30-common/prebuilt/fillers:root/fillers \
+#    device/semc/msm7x30-common/prebuilt/fillers:root/fillers \ #37kb
+#    device/semc/msm7x30-common/prebuilt/fillers:root/filler \ #500kb
 
 
 #Audio
